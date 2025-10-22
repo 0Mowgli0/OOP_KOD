3df35eaa55ced605bfa777c5d4db2abd1a7f1826
Hampus Lind (Student) <h24hamli@du.se>
2025-10-22 12:34:29 +0200

    Jonas/Hampus

---
+++ OOP_KOD/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_KOD;
+
+public sealed class Event
+{
+    public int Id { get; }
+    public string Name { get; }
+    public DateTime StartsAt { get; }
+    public DateTime ReleaseAt { get; }
+    public Arena Arena { get; }
+
+    public Event(int id, string name, DateTime startsAt, DateTime releaseAt, Arena arena)
+    {
+        Id = id;
+        Name = name;
+        StartsAt = startsAt;
+        ReleaseAt = releaseAt;
+        Arena = arena;
+    }
+}
