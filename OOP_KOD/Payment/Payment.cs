d8d0ca758fc1ca5b291d8828c69bf92061f0584d
jjonas97770 <jonas_pettersson97@hotmail.com>
2025-10-29 12:56:37 +0100

    rahams del
    rahams del

---
+++ OOP_KOD/Payment/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_KOD.Payment
+{
+    public abstract class Payment : IPayment
+    {
+        public double Amount { get; }
+        protected Payment(double amount) { Amount = amount; }
+        public abstract void Pay();
+    }
+}
